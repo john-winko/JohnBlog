b9c6a0bdf740049dee345bda4890f344c4c744b4
John Winko <john.winko@gmail.com>
2021-11-30 20:11:46 -0600

    save points

---
+++ Models/BlogUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace JohnBlog.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [BindProperty]
+        public BlogImage Image { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string FacebookUrl { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string TwitterUrl { get; set; }
+
+        public virtual ICollection<Blog> Blogs { get; set; } = new List<Blog>();
+        public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+    }
+}
