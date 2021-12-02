8f355e327b8a1684e77de321a8d229c635e94ca3
John Winko <john.winko@gmail.com>
2021-12-01 19:16:22 -0600

    Updated BlogUser to be the identity and changed Program.cs to use it. Added DataService to seed the database. injected dataservice on startup but forgot to change identity to bloguser. having issue with identity role dependencies now.

---
+++ Services/DataService.cs
@@ -0,0 +1,67 @@
+﻿using JohnBlog.Data;
+using JohnBlog.Enums;
+using JohnBlog.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace JohnBlog.Services
+{
+    public class DataService
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<BlogUser> userManager;
+
+        public DataService(RoleManager<IdentityRole> roleManager, UserManager<BlogUser> userManager, ApplicationDbContext dbContext)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+            this.dbContext = dbContext;
+        }
+
+        public async Task SeedDatabaseAsync()
+        {
+            await dbContext.Database.EnsureCreatedAsync();
+            await SeedRolesAsync();
+            await SeedUsersAsync();
+        }
+
+        private async Task SeedRolesAsync()
+        {
+            if (dbContext.Roles.Any()) return;
+
+            foreach (var role in Enum.GetNames(typeof(BlogRole)))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+        private async Task SeedUsersAsync()
+        {
+            if (dbContext.Users.Any()) return;
+
+            var adminUser = new BlogUser()
+            {
+                Email = "john.winko@gmail.com",
+                UserName = "Admin-John",                
+                FirstName = "John",
+                LastName = "Winko",
+                PhoneNumber = "(904) 703-4856",
+                EmailConfirmed = true
+            };
+            await userManager.CreateAsync(adminUser, "asdfASDF1234!@#$");
+            await userManager.AddToRoleAsync(adminUser, BlogRole.Administrator.ToString());
+
+            var moderatorUser = new BlogUser()
+            {
+                Email = "eternal81@msn.com",
+                UserName = "Mod-John",
+                FirstName = "John",
+                LastName = "Winko",
+                PhoneNumber = "(904) 703-4856",
+                EmailConfirmed = true
+            };           
+            await userManager.CreateAsync(moderatorUser, "Abc&123!");
+            await userManager.AddToRoleAsync(moderatorUser, BlogRole.Moderator.ToString());
+        }
+    }
+}
