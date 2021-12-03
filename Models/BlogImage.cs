aab414a39c071bca8719bdc03acb2838aedc7605
John Winko <john.winko@gmail.com>
2021-12-03 11:49:25 -0600

    Made an owned class of BlogImage and associated it on the BlogUser

---
+++ Models/BlogImage.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JohnBlog.Models;
+
+[Owned]
+public class BlogImage
+{
+    public byte[]? ImageData { get; set; }
+    public string? ContentType { get; set; }
+}
