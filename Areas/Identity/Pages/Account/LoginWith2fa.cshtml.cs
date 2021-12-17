683971ec0e0843492ce8220ef045196c51a0b70f
John Winko <john.winko@gmail.com>
2021-12-16 18:27:40 -0600

    Suppressing null warnings for sanity

--- Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -2,18 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
 using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using JohnBlog.Models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+// ReSharper disable LogMessageIsSentenceProblem
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedVariable
 
 namespace JohnBlog.Areas.Identity.Pages.Account
 {
