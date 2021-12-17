8175ef78e2b660c4bc8e680764d4f2d185de3566
John Winko <john.winko@gmail.com>
2021-12-17 16:44:40 -0600

    Created user table for editing for admins

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using JohnBlog.Data;
+using JohnBlog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JohnBlog.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<BlogUser> _userManager;
+
+    public AdminController(ApplicationDbContext context, UserManager<BlogUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+    
+// GET
+    public IActionResult Index()
+    {
+        var users = _context.Users;
+        return View(users);
+    }
+
+    // public IActionResult Edit(string blogUserId)
+    // {
+    //     
+    // }
+    // TODO: add edit / delete functionality 
+}
