ac6081e32a00dd4b6defefb26a30c209372aa268
John Winko <john.winko@gmail.com>
2021-12-18 08:49:41 -0600

    Found proper linq to join users with roles. implemented over using usermanager to populate each so only one database call is needed

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -22,11 +22,47 @@ public class AdminController : Controller
     [Authorize(Roles = "Administrator")]
     public IActionResult GetRoles()
     {
-        var users = _context.Users
+        var users = GetBlogUsersWithRoles();
+        
+        return View(users);
+    }
+
+    private Dictionary<BlogUser, List<IdentityRole>> GetBlogUsersWithRoles()
+    {
+        // https://stackoverflow.com/questions/51004516/net-core-2-1-identity-get-all-users-with-their-associated-roles
+        var res = _context.Users
             .OrderByDescending(p => p.EmailConfirmed)
             .ThenByDescending(p => p.Email)
-            .ToList();
-        return View(users);
+            // left outer join for UserId->RoleIds, returns DefaultIfEmpty in the collectionSelector  
+            .SelectMany(
+                user => _context.UserRoles
+                    .Where(userRoleMapEntry => user.Id == userRoleMapEntry.UserId)
+                    .DefaultIfEmpty(),
+                (user, roleMapEntry) => new {User = user, RoleMapEntry = roleMapEntry})
+            // left outer join for RoleId->RoleNames
+            .SelectMany(
+                x => _context.Roles.Where(role => role.Id == x.RoleMapEntry.RoleId).DefaultIfEmpty(),
+                (x, role) => new {x.User, Role = role})
+            // runs the queries and sends us back into EF Core LINQ world
+            .ToList() 
+            .Aggregate(
+                // seed
+                new Dictionary<BlogUser, List<IdentityRole>>(),
+                // function
+                (blogUser, roles) =>
+                {
+                    // safely ensure the user entry is configured
+                    blogUser.TryAdd(roles.User, new List<IdentityRole>());
+                    // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                    if (null != roles.Role)
+                    {
+                        blogUser[roles.User].Add(roles.Role);
+                    }
+                    return blogUser;
+                },
+                // result
+                x => x);
+        return res;
     }
 
     public async Task<IActionResult> RemoveRole(string? blogUserId, string? roleName)
@@ -48,14 +84,14 @@ public class AdminController : Controller
         return RedirectToAction("GetRoles");
     }
 
-    
+
     public async Task<IActionResult> DeleteUser(string? blogUserId)
     {
         if (blogUserId is null) return NotFound();
         var user = await _userManager.FindByIdAsync(blogUserId);
         return View(user);
     }
-    
+
     [HttpPost, ActionName("DeleteUser")]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> ConfirmDeleteUser(string? id)
@@ -65,5 +101,4 @@ public class AdminController : Controller
         await _userManager.DeleteAsync(user);
         return RedirectToAction("GetRoles");
     }
-    // TODO: create custom view/model that joins roles with users... injecting and repeatedly using usermanager = BAD!
 }
